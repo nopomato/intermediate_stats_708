67822ba25f4ddbb4ad60414c001d2ab067115560
Steven Vannoy <steven.vannoy@gmail.com>
2016-01-31 13:51:41 -0500

    Added homework #2 outline
    Added data file for homework #2

---
+++ homework_session2.R
@@ -0,0 +1,44 @@
+##
+# Homework for Session 2 - CSP 708, Spring 2016
+#
+
+# Download file "creativity_survey_dat.csv" from github.com:svannoy/intermediate_stats_708
+
+# Read creativity_survey_dat.csv into R
+
+# Re-Create Descriptive Statistics for:
+
+
+# Age
+
+# Gender
+
+# Academic class
+
+# GPA
+
+# Race
+
+# Sexual Orientation
+
+# Create descriptive statistics (mean, median, sd, range, boxplot, histogram )for
+
+# phq9 (Patient Health Care Questionnaire 9 item scale)
+
+# phqSI (Item 9 from the phq9, indicates presence of suicide ideation, 0 = no ideation)
+
+# gad7 (Genearlized Anxiety Disorder 7 item scale)
+
+# sbq (Suicide Behaviors Questionnaire)
+
+# sbqQ1 (Item 1 - Suicide Behaviors Questionnaire)
+
+# sbqQ2 (Item 1 - Suicide Behaviors Questionnaire)
+
+# sbqQ3 (Item 1 - Suicide Behaviors Questionnaire)
+
+# sbqQ4 (Item 1 - Suicide Behaviors Questionnaire)
+
+# rfl (Reasons for Living)
+
+# scs (Suicide Cognitions Scale)
